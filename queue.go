ab18b86804af470b1224712ee19ee9e59e68eeb7
monochromegane <dev.kuro.obi@gmail.com>
2018-11-08 00:50:40 +0900

    Lock only for conflict resource.

--- queue.go
+++ queue.go
@@ -32,22 +32,13 @@ func (q *queue) full() bool {
 }
 
 func (q *queue) empty() bool {
-	defer q.mu.RUnlock()
-	q.mu.RLock()
-
 	return q.counter == 0
 }
 
 func (q *queue) enqueue() {
-	defer q.mu.Unlock()
-	q.mu.Lock()
-
 	q.counter++
 }
 
 func (q *queue) dequeue() {
-	defer q.mu.Unlock()
-	q.mu.Lock()
-
 	q.counter--
 }

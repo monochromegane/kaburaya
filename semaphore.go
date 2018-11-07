ef03bfe83e03276fec642d5b038f1017f415fee8
monochromegane <dev.kuro.obi@gmail.com>
2018-11-08 00:56:55 +0900

    Implement wait and signal using elastic channel.

--- semaphore.go
+++ semaphore.go
@@ -2,18 +2,28 @@ package kaburaya
 
 // Semaphore represents an elastic semaphore.
 type Semaphore struct {
+	ch *elasticChannel
 }
 
 // NewSem returns a semaphore.
 func NewSem() *Semaphore {
-	return &Semaphore{}
+	return &Semaphore{
+		ch: newElasticChannel(1),
+	}
 }
 
 // Wait decrements semaphore. If semaphore will be negative,
 // it blocks the process.
 func (s *Semaphore) Wait() {
+	s.ch.send <- struct{}{}
 }
 
 // Signal increments semaphore.
 func (s *Semaphore) Signal() {
+	<-s.ch.receive
+}
+
+// Stop finalize resources.
+func (s *Semaphore) Stop() {
+	s.ch.stop()
 }

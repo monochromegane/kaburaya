d738a25ed7cbe434ab5e5cad5a8647fe1f81f33c
monochromegane <dev.kuro.obi@gmail.com>
2018-11-20 11:59:34 +0900

    Implement classical feedback controller.

--- controller.go
+++ controller.go
@@ -23,3 +23,30 @@ func (c *queueController) Compute(current float64) float64 {
 	c.previous = cur
 	return -1.0
 }
+
+type pidController struct {
+	target   float64
+	kp       float64
+	ki       float64
+	kd       float64
+	i        float64
+	previous float64
+}
+
+func newPIDController(target, kp, ki, kd float64) *pidController {
+	return &pidController{
+		target: target,
+		kp:     kp,
+		ki:     ki,
+		kd:     kd,
+	}
+}
+
+func (c *pidController) Compute(feedback float64) float64 {
+	e := (c.target - feedback)
+	c.i += e
+	d := e - c.previous
+	c.previous = e
+
+	return (c.kp * e) + (c.ki * c.i) + (c.kd * d)
+}
